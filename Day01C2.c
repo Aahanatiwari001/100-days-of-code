80457baa7ffa557b7f980f573d6ab43988173bb6
Aahanatiwari001 <aahana.32758@stu.upes.ac.in>
2026-08-10 20:00:00 +0530

    Day01 programs

---
+++ Day01C2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+  int a=23,b=12;
+  int sum=a+b;
+  int diff=a-b;
+  int mult=a*b;
+  int div=a/b;
+  int rem=a%b;
+  printf("The addition of a and b is %d",sum);
+  printf("The subtraction of a and b is %d",diff);
+  printf("The dividion of and b is %d",div);
+  printf("The multiplication of and b is %d",mult);
+  printf("The remainder of a and b is %d",rem);
+  return 0;
+}
